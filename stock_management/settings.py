434551dafb3d0266e735de2a4bfe09c75516e260
prinzz1208 <priyanshu81212@gmail.com>
2021-03-04 22:08:03 +0530

    Reconfigured

--- stock_management/settings.py
+++ stock_management/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    # 3rd party libraries
+    'rest_framework',
+
+    # my apps.
+    'users'
 ]
 
 MIDDLEWARE = [
