06f67f491eff8165d523ee96832478c2abd2a611
prinzz1208 <priyanshu81212@gmail.com>
2021-03-06 20:12:02 +0530

    Auth system complete

--- users/authentication.py
+++ users/authentication.py
@@ -1,8 +1,11 @@
 import datetime
+
 import jwt
 from django.conf import settings
 from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication
+
+from users.models import User
 
 
 def generateAccessToken(user):
@@ -11,16 +14,23 @@ def generateAccessToken(user):
         'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
         'iat': datetime.datetime.utcnow()
     }
-    return jwt.encode(payload=payload, algorithm='HS256').decode('utf-8')
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
 
 
 class JWTAuthentication(BaseAuthentication):
     def authenticate(self, request):
         token = request.COOKIES.get('jwt')
+        print(token)
         if not token:
             return None
 
         try:
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
-        except:
+        except jwt.exceptions.ExpiredSignatureError:
             raise exceptions.AuthenticationFailed('Unauthenticated')
+
+        user = User.objects.filter(id=payload.get('user_id')).first()
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found')
+
+        return (user, None)
